8c940b58b4a97a9046c0a680e9f8b0b7b6988410
Valeri Karpov <valkar207@gmail.com>
2012-11-17 20:11:02 +0000

    Some basic factory stuff, doesn't quite work yet >.<

--- routes/tags.js
+++ routes/tags.js
@@ -1,28 +1,18 @@
+var MongoLookFactory = require('../factories/MongoLookFactory.js').MongoLookFactory;
 
 /*
- * GET tags
+ * GET tags.jsonp?url=<url>
  */
 
-exports.get = function(req, res){
-  var testLook = {
-    title: "test",
-    url: "ascotproject.com/test.jpg",
-    tags: [{
-      position: {x: 32, y: 32}, 
-      index: 1,
-      product: {
-        name: "Big Bang Gold Leopard", 
-        brand: "Hublot",
-        type: "watch",
-        price: 1000000,
-        id: 1,
-        buy_link: "http://www.hublot.com/watch/big-bang/98/gold-leopard"
-      }
-    },{
-
-    }]
-  };
-
-
-  res.jsonp(JSON.stringify(testLook));
+exports.get = function(req, res) {
+  var mongoLookFactory = new MongoLookFactory();
+  mongoLookFactory.buildFromUrl(req.query["url"], function(error, look) {
+    if (error || !look) {
+      // TODO: error page
+      res.render('index', { title : "Errror!" });
+      console.log(JSON.stringify(error));
+    } else {
+      res.jsonp(JSON.stringify(look));
+    }
+  });
 };
