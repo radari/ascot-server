52a3babc954258dabc405e9c4c8512e1ba22865f
Matt Green <mgreen9@gmail.com>
2012-11-17 14:18:44 -0500

    switched indentation to tags

--- routes/tags.js
+++ routes/tags.js
@@ -1,25 +1,25 @@
 
 exports.get = function(req, res){
 
-	var testLook = {
-		title: "test",
-		url: "ascotproject.com/test.jpg",
-		tags: [{
-			position: {x: 32, y: 32}, 
-			index: 1,
-			product: {
-				name: "Big Bang Gold Leopard", 
-				brand: "Hublot",
-				type: "watch",
-				price: 1000000,
-				id: 1,
-				buy_link: "http://www.hublot.com/watch/big-bang/98/gold-leopard"
-			}
-		},{
+  var testLook = {
+    title: "test",
+    url: "ascotproject.com/test.jpg",
+    tags: [{
+      position: {x: 32, y: 32}, 
+      index: 1,
+      product: {
+        name: "Big Bang Gold Leopard", 
+        brand: "Hublot",
+        type: "watch",
+        price: 1000000,
+        id: 1,
+        buy_link: "http://www.hublot.com/watch/big-bang/98/gold-leopard"
+      }
+    },{
 
-		}]
-	};
+    }]
+  };
 
 
-	res.jsonp(JSON.stringify(testLook));
+  res.jsonp(JSON.stringify(testLook));
 };
