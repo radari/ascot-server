327602394a876065e3012747de54cd6bf6c89091
Valeri Karpov <valkar207@gmail.com>
2012-11-18 12:54:13 +0000

    Make product search backend work with autocomplete frontend

--- routes/products.js
+++ routes/products.js
@@ -25,7 +25,15 @@ exports.get = function(req, res) {
       regexps.push(new RegExp('^' + sp[i], 'i'));
     }
     Product.find().where('search').all(regexps).exec(function(error, results) {
-      res.json(results);
+      var ret = {};
+      ret["query"] = req.query["query"];
+      ret["suggestions"] = [];
+      ret["data"] = [];
+      for (var i = 0; i < results.length; ++i) {
+        ret["suggestions"].push(results[i].name + ' from ' + results[i].brand + ' (' + results[i].type + ')');
+        ret["data"].push(results[i]);
+      }
+      res.json(ret);
     });
   } else {
     res.json([]);
