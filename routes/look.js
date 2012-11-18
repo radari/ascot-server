c22fabc662f73eabeaaa9c32f3b37ddb60a85b43
Valeri Karpov <valkar207@gmail.com>
2012-11-18 00:55:54 +0000

    Couple changes for look.js

--- routes/look.js
+++ routes/look.js
@@ -7,15 +7,17 @@ var MongoLookFactory = require('../factories/MongoLookFactory.js').MongoLookFact
 var fs = require('fs');
 
 
-exports.get = function(req, res){
-
+exports.get = function(req, res) {
   // retrieve database
-  var look = new MongoLookFactory()
+  var look = new MongoLookFactory();
 
   look.buildFromId(req.params.id, function(error, result){
-    if (error || !result) {
+    if (error) {
       console.log("Error " + JSON.stringify(error) + "---");
       callback(error, null);
+    } else if (!result) {
+      // TODO : invalid ID, show page not found
+      res.render('index', { title : "ERROR" });
     } else {
       // render layout
       console.log(JSON.stringify(result));
@@ -24,28 +26,23 @@ exports.get = function(req, res){
   });
 };
 
-exports.upload = function(req, res){
-    console.log(req.body);
-    console.log(req.files);
+exports.upload = function(req, res) {
+  console.log(req.body);
+  console.log(req.files);
 
-    var look = new MongoLookFactory()
+  var look = new MongoLookFactory();
 
-    // get the temporary location of the file
-    var tmp_path = req.files.thumbnail.path;
-    // set where the file should actually exists - in this case it is in the "images" directory
-    var target_path = './public/images/uploads/' + req.files.thumbnail.name;
-    // move the file from the temporary location to the intended location
-    fs.rename(tmp_path, target_path, function(err) {
-        if (err) throw err;
-        // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
-        fs.unlink(tmp_path, function() {
-            if (err) throw err;
-
-            look.newLook(req.files.thumbnail.name, target_path.replace(/\.\/public/, ""), function(){
-
-            });
-
-            res.redirect('/');
-        });
+  // get the temporary location of the file
+  var tmp_path = req.files.thumbnail.path;
+  // set where the file should actually exists - in this case it is in the "images" directory
+  var target_path = './public/images/uploads/' + req.files.thumbnail.name;
+  // move the file from the temporary location to the intended location
+  fs.rename(tmp_path, target_path, function(err) {
+    // delete the temporary file, so that the explicitly set temporary upload dir does not get filled with unwanted files
+    fs.unlink(tmp_path, function() {
+      look.newLook(req.files.thumbnail.name, target_path.replace(/\.\/public/, ""), function(){
+        res.redirect('/');
+      });
     });
+  });
 };
