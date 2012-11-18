ef3a40eee25dacb883cd5714c0e0e4f5c7b4eb3e
Matt Green <mgreen9@gmail.com>
2012-11-18 12:00:12 -0500

    fixed buy link syntax issue

--- public/javascript/animationJS.js
+++ public/javascript/animationJS.js
@@ -38,7 +38,7 @@ $(document).ready(function (){
 
             var itemDescription = $("<div class='item-description'></div>")
             itemDescription.html(e.product.name + "<br/><a target='_blank' href='" + 
-              e.product.buy_link + "'>"+e.product.buy_link+"</a><br/>$" + e.product.price)
+              e.product.buy_link + "'>"+e.product.buyLink+"</a><br/>$" + e.product.price)
 
             itemDescription.appendTo(tag)
             itemDescription.hide()
