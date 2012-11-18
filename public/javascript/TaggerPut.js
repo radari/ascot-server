798c0a3c58c5390c310b91db4bae5de06216006d
Valeri Karpov <valkar207@gmail.com>
2012-11-18 15:18:03 +0000

    Moar taggar

--- public/javascript/TaggerPut.js
+++ public/javascript/TaggerPut.js
@@ -1,82 +1,60 @@
+
 $(document).ready(function() {
-
-  var tagData = []
-
-  var container = $('#look')
-  var image = $('img.uploadedImg')
-  
-
-  image.load(function(){
-
-    var tag = {}
-
-    var height = image.height()
-    var width = image.width()
-
-    console.log(height)
-
-    container.height(height)
-    container.width(width)
-
-    image.on("click", function(e){
-      console.log(e)
-
-      var overlay = container.find('#overlay')
-
-      tag.position = {x: e.offsetX, y: e.offsetY},
-      tag.index = tagData.length + 1
-      
-      overlay.css('position', 'relative');
-      overlay.css('left', (e.offsetX+10) + "px");
-      overlay.css('top', e.offsetY + "px");
-      overlay.show();
-
-      var auto = $("#productSearch").autocomplete({
-        serviceUrl : '/products.json',
-        minChars : 2,
-        delimiter : /(,|;)\s*/, // regex or character
-        zIndex : 10000,
-        deferRequestBy : 0,
-        onSelect : function(str, product) {
-          alert("You selected " + JSON.stringify(product));
-          json.tags.push({ index : json.tags.length + 1,
-                           position : { x : (event.pageX - $(el).parent().offset().left),
-                                        y : (event.pageY - $(el).parent().offset().top) },
-                           product : product });
-          auto.disable();
+  $("img[ascot_key][ascot]").each(function(i, el) {
+    $(el).load(function() {
+      $.ajax({
+        type : 'GET',
+        url : '/tags.jsonp?url=' + encodeURIComponent($(el).attr("src")),
+        async : true,
+        success : function(json) {
+          alert(JSON.stringify(json));
+          var editting = false;
+          //ascot.doDisplay(el, json);
+          //$("#json_content").val(JSON.stringify(json));
+          $(el).wrap('<div style="position: relative; width: ' + $(el).width() + 'px; height: ' + $(el).height() + 'px" />');
+          for (var i = 0; i < json.tags.length; ++i) {
+            $(el).parent().append('<div class="item" style="position: absolute; left: '
+                + (json.tags[i].position.x) + 'px; top: '
+                + (json.tags[i].position.y) + 'px">'
+                + (json.tags[i].index) + '</div>');
+          }
+          $(el).parent().click(function(event) {
+            if (editting) {
+              return;
+            }
+            editting = true;
+            $(el).parent().append('<div class="item" style="position: absolute; left: '
+                + (event.pageX - $(el).parent().offset().left) + 'px; top: '
+                + (event.pageY - $(el).parent().offset().top) + 'px">'
+                + (json.tags.length + 1) + '</div>');
+            $("#overlay").css('left', (event.pageX - $(el).parent().offset().left + 20) + 'px');
+            $("#overlay").css('top', (event.pageY - $(el).parent().offset().top + 30) + 'px');
+            $("#overlay").show();
+            var auto = $("#productSearch").autocomplete({
+              serviceUrl : '/products.json',
+              minChars : 2,
+              delimiter : /(,|;)\s*/, // regex or character
+              zIndex : 1000,
+              deferRequestBy : 0,
+              onSelect : function(str, product) {
+                //alert("You selected " + JSON.stringify(product));
+                json.tags.push({ index : json.tags.length + 1,
+                                 position : { x : (event.pageX - $(el).parent().offset().left),
+                                              y : (event.pageY - $(el).parent().offset().top) },
+                                 product : product });
+                $("#productSearch").val('');
+                $("#overlay").hide();
+                auto.disable();
+                $("#json_content").val(JSON.stringify(json));
+                editting = false;
+              }
+            }).enable();
+          });
+        },
+        error : function(e) {
         }
-      }).enable();
-
+      });
     });
-
-    // add new tag
-    $("#add_tag").on("click", function(){
-      var productInput = $("#productSearch")
-      tag.product = {id: productInput.val()}
-      productInput.val("")
-
-      tagData.push(tag)
-
-      var newTag = $("<div class='item'></div>")
-      newTag.text(tag.product.id)
-      newTag.css('left', tag.position.x - 10)
-      newTag.css('top', tag.position.y)
-
-      var t = newTag.css('z-index')
-      newTag.css('z-index', t+1)
-      container.append(newTag)
-
-
-    });
-
-  });
-
-
-
-
-  // Submit tagData
-  $("#json_content_submit").click(function() {
-    TaggerPut($("#myKey").val(), $("#myId").val(), tagData);
   });
 });
 
@@ -92,21 +70,3 @@ var TaggerPut = function(key, look, data) {
     failure: function() {}
   });
 };
-
-function doSomething(e) {
-  var posx = 0;
-  var posy = 0;
-  if (!e) var e = window.event;
-  if (e.pageX || e.pageY)   {
-    posx = e.pageX;
-    posy = e.pageY;
-  }
-  else if (e.clientX || e.clientY)  {
-    posx = e.clientX + document.body.scrollLeft
-      + document.documentElement.scrollLeft;
-    posy = e.clientY + document.body.scrollTop
-      + document.documentElement.scrollTop;
-  }
-  // posx and posy contain the mouse position relative to the document
-  // Do something with this information
-}
