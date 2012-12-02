ccf31b95603d123ae1ee78a5cd25323040045839
Valeri Karpov <valkar207@gmail.com>
2012-12-02 01:12:34 +0000

    Add source tag to model

--- models/Look.js
+++ models/Look.js
@@ -14,6 +14,7 @@ exports.LookSchema = new Mongoose.Schema({
   title : String,
   url : String,
   random : { type : Number, default : Math.random() },
+  source : { type : String, default : "http://ascotproject.com" },
   search : [String],
   tags : [
     {
