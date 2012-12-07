4d3cd9945f70c0e1101a6f8cc67f3138b3f24ed9
Valeri Karpov <valkar207@gmail.com>
2012-12-06 22:32:57 -0500

    Much awesome for tagger - now can do double-click to delete and basic autocomplete

--- public/javascripts/TaggerViewController.js
+++ public/javascripts/TaggerViewController.js
@@ -12,18 +12,72 @@ function TaggerViewController($scope, $http, ImageOffsetService) {
   this.$scope = $scope;
 
   $scope.idsToLooks = {};
+  $scope.idsToEditTag = {};
+  $scope.idsToSearchResults = {};
 
   // Initial load of look
   $scope.loadLook = function(id) {
     $scope.idsToLooks[id] = { _id : id, tags : [] };
-    $http.get('/tags.jsonp?id=' + encodeURIComponent(id)).success(function(look) {
-      $scope.idsToLooks[id] = look;
-    }).error(function(error) { alert(error); });
+    $http.get('/tags.jsonp?id=' + encodeURIComponent(id)).success(
+        function(look) {
+          $scope.idsToLooks[id] = look;
+          $scope.idsToEditTag[id] = null;
+          $scope.idsToSearchResults[id] = [];
+        });
   };
 
   // Add a tag
   $scope.addTag = function(id, pageX, pageY) {
     var offset = ImageOffsetService.getOffset(id);
-    $scope.idsToLooks[id].tags.push({ index : $scope.idsToLooks[id].tags.length + 1, position : { x : (pageX - offset.x), y : (pageY - offset.y) } });
+    var newTag =
+        { index : $scope.idsToLooks[id].tags.length + 1,
+          position : { x : (pageX - offset.x), y : (pageY - offset.y) } };
+
+    $scope.idsToLooks[id].tags.push(newTag);
+    $scope.editTaggedProduct(id, newTag);
+    return newTag;
+  };
+
+  // Start editting a tag
+  $scope.editTaggedProduct = function(id, tag) {
+    $scope.idsToEditTag[id] = tag;
+  };
+
+  // Check if we're in editting state
+  $scope.isEdittingTag = function(id) {
+    return $scope.idsToEditTag[id] != null;
+  };
+
+  // Search for products
+  $scope.findProducts = function(id, search) {
+    $http.get('/products.json?query=' + encodeURIComponent(search)).success(
+        function(results) {
+          $scope.idsToSearchResults[id] = results.data;
+        });
+  };
+
+  // Finish editting tagged product
+  $scope.setTaggedProduct = function(id, product) {
+    if ($scope.isEdittingTag(id)) {
+      $scope.idsToEditTag[id].product = product._id;
+      $scope.idsToEditTag[id] = null;
+    }
+  };
+
+  // Delete a tag
+  $scope.deleteTag = function(id, tag) {
+    var index = -1;
+    for (var i = 0; i < $scope.idsToLooks[id].tags.length; ++i) {
+      if ($scope.idsToLooks[id].tags[i] == tag) {
+        index = i;
+        break;
+      }
+    }
+    if (index != -1) {
+      $scope.idsToLooks[id].tags.remove(index);
+      for (var i = 0; i < $scope.idsToLooks[id].tags.length; ++i) {
+        $scope.idsToLooks[id].tags[i].index = i + 1;
+      }
+    }
   };
 }
--- public/tests/TaggerViewController.test.js
+++ public/tests/TaggerViewController.test.js
@@ -6,11 +6,20 @@ describe('TaggerViewController', function() {
     $httpBackend = $injector.get('$httpBackend');
     scope = $rootScope.$new();
 
-    v = $controller(TaggerViewController, {$scope : scope, ImageOffsetService : { getOffset : function(id) { return { x : 10, y : 5 }; } } });
+    v = $controller(TaggerViewController,
+        { $scope : scope,
+          ImageOffsetService :
+              { getOffset :
+                  function(id) {
+                    return { x : 10, y : 5 };
+                  }
+               }
+        });
   }));
 
   it('should make an HTTP callout to get its data', function() {
-    $httpBackend.expectGET('/tags.jsonp?id=1234').respond({ tags : [{index : 1}] });
+    $httpBackend.expectGET('/tags.jsonp?id=1234').
+        respond({ tags : [{index : 1}] });
 
     scope.loadLook('1234');
     $httpBackend.flush();
@@ -19,7 +28,8 @@ describe('TaggerViewController', function() {
   });
 
   it('should add tag successfully', function() {
-    $httpBackend.expectGET('/tags.jsonp?id=1234').respond({ tags : [{index : 1}] });
+    $httpBackend.expectGET('/tags.jsonp?id=1234').
+        respond({ tags : [{index : 1}] });
 
     scope.loadLook('1234');
     $httpBackend.flush();
@@ -29,5 +39,39 @@ describe('TaggerViewController', function() {
     expect(scope.idsToLooks['1234'].tags[1].index).toBe(2);
     expect(scope.idsToLooks['1234'].tags[1].position.x).toBe(15);
     expect(scope.idsToLooks['1234'].tags[1].position.y).toBe(10);
+  });
+
+  it('should save product index after done tagging', function() {
+    $httpBackend.expectGET('/tags.jsonp?id=1234').
+        respond({ tags : [] });
+
+    scope.loadLook('1234');
+    $httpBackend.flush();
+    
+    var tag1 = scope.addTag('1234', 25, 15);
+    expect(scope.idsToEditTag['1234']).toBe(tag1);
+    
+    scope.setTaggedProduct('1234', { _id : '567' });
+    expect(scope.idsToEditTag['1234']).toBe(null);
+    expect(scope.idsToLooks['1234'].tags.length).toBe(1);
+    expect(scope.idsToLooks['1234'].tags[0].product).toBe('567');
+  });
+
+  it('should delete tag successfully including updating indices', function() {
+    $httpBackend.expectGET('/tags.jsonp?id=1234').
+        respond({ tags : [] });
+
+    scope.loadLook('1234');
+    $httpBackend.flush();
+
+    var tag1 = scope.addTag('1234', 25, 15);
+    var tag2 = scope.addTag('1234', 50, 25);
+    var tag3 = scope.addTag('1234', 75, 50);
+    expect(scope.idsToLooks['1234'].tags.length).toBe(3);
+    
+    scope.deleteTag('1234', tag2);
+    expect(scope.idsToLooks['1234'].tags.length).toBe(2);
+    expect(tag1.index).toBe(1);
+    expect(tag3.index).toBe(2);
   });
 });
