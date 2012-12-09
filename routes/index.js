a227002385871027de37aa3b0f7fbd5371f8073e
Valeri Karpov <valkar207@gmail.com>
2012-12-08 20:07:03 -0500

    Some more stylistic changes

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,15 @@
 
 /*
- * GET home page.
+ * GET /
  */
 
 exports.index = function(req, res){
   res.render('index', { title: 'Ascot' });
 };
+
+/*
+ * GET /about
+ */
 
 exports.about = function(req, res){
   res.render('about', {title: 'About'});
--- routes/products.js
+++ routes/products.js
@@ -17,6 +17,10 @@ var Product = db.model('products', ProductSchema);
 var LookSchema = require('../models/Look.js').LookSchema;
 var Look = db.model('looks', LookSchema);
 
+/*
+ * GET /products.json
+ */
+
 exports.get = function(req, res) {
   if (req.query["query"]) {
     var sp = req.query["query"].toLowerCase().split(/\s+/);
@@ -24,21 +28,28 @@ exports.get = function(req, res) {
     for (var i = 0; i < sp.length; ++i) {
       regexps.push(new RegExp('^' + sp[i], 'i'));
     }
-    Product.find().where('search').all(regexps).limit(7).exec(function(error, results) {
-      var ret = {};
-      ret["query"] = req.query["query"];
-      ret["suggestions"] = [];
-      ret["data"] = [];
-      for (var i = 0; i < results.length; ++i) {
-        ret["suggestions"].push(results[i].name + ' from ' + results[i].brand + ' (' + results[i].type + ')');
-        ret["data"].push(results[i]);
-      }
-      res.json(ret);
-    });
+    Product.find().where('search').all(regexps).limit(7).
+        exec(function(error, results) {
+          var ret = {};
+          ret["query"] = req.query["query"];
+          ret["suggestions"] = [];
+          ret["data"] = [];
+          for (var i = 0; i < results.length; ++i) {
+            ret["suggestions"].push(
+                results[i].name + ' from ' + results[i].brand
+                + ' (' + results[i].type + ')');
+            ret["data"].push(results[i]);
+          }
+          res.json(ret);
+        });
   } else {
     res.json([]);
   }
 };
+
+/*
+ * GET /products/:id/looks
+ */
 
 exports.looks = function(req, res) {
   Product.findOne({ _id : req.params.id }, function(error, product) {
@@ -55,6 +66,10 @@ exports.looks = function(req, res) {
     }
   });
 };
+
+/*
+ * GET /filters.json
+ */
 
 exports.filters = function(req, res) {
   var ret = {};
