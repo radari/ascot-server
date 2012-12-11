cd1381b8488f4f2f0ae906623c27498159cda669
Valeri Karpov <valkar207@gmail.com>
2012-12-11 00:17:14 -0500

    Add fix for #22 to migratedb - this will fix all products for next pull

--- data/migratedb/migratedb.js
+++ data/migratedb/migratedb.js
@@ -1,11 +1,23 @@
 var Mongoose = require('mongoose');
 var db = Mongoose.createConnection('localhost', 'ascot');
 
-var LookSchema = require('../models/Look.js').LookSchema;
+var LookSchema = require('../../models/Look.js').LookSchema;
 var Look = db.model('looks', LookSchema);
+
+var ProductSchema = require('../../models/Product.js').ProductSchema;
+var Product = db.model('products', ProductSchema);
 
 var randomTo2D = function(look) {
   look.random = [Math.random(), 0];
+};
+
+var fixShopstyleUrls = function(product) {
+  if (product.buyLink.indexOf('http://api.shopstyle.com/action/apiVisitRetailer?id=') == 0) {
+    var index = product.buyLink.indexOf('pid');
+    product.buyLink = product.buyLink.substring(0, index) + '&' + product.buyLink.substring(index);
+    return true;
+  }
+  return false;
 };
 
 Look.find({}, function(error, looks) {
@@ -14,5 +26,15 @@ Look.find({}, function(error, looks) {
     randomTo2D(looks[i]);
     looks[i].save();
   }
-  console.log("DONE");
+  Product.find({}, function(error, products) {
+    for (var i = 0; i < products.length; ++i) {
+      if (fixShopstyleUrls(products[i])) {
+        console.log(products[i].buyLink);
+      }
+      products[i].save();
+    }
+    console.log("DONE");
+  });
 });
+
+
