92d15124673ab56465d176647130a12282fceda2
Valeri Karpov <valkar207@gmail.com>
2013-01-23 14:09:02 -0500

    Remove unused files including display_image.js #57

--- public/javascripts/all_looks.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-     // 
-      // Apply fillwidth passing in the widths stored above to avoid initial flickering.
-      // 
-      
-      $('.all_looks_wrapper').fillwidth();
-
-    });
