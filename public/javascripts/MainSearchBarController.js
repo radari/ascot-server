c0a5ab4ddb4112436c8e7bd001b7126037904c0f
Valeri Karpov <valkar207@gmail.com>
2013-01-25 16:56:12 -0500

    MainSearchBarController has now replaced search_autocomplete

--- public/javascripts/MainSearchBarController.js
+++ public/javascripts/MainSearchBarController.js
@@ -11,15 +11,29 @@
 function MainSearchBarController($scope, $http, $redirect) {
   $scope.mainSearch = "";
   $scope.results = [];
-  $scope.$redirect = $redirect;
+  $scope.suggestions = [];
 
   $scope.updateResults = function() {
     if ($scope.mainSearch.length >= 2) {
       $http.get('/filters.json?query=' +
           encodeURIComponent($scope.mainSearch)).
           success(function(data) {
-            $scope.results = data;
+            $scope.results = data.data;
+            $scope.suggestions = data.suggestions;
           });
+    } else {
+      $scope.results = [];
+      $scope.suggestions = [];
+    }
+  };
+
+  $scope.onSelected = function(filter) {
+    if (filter.type == 'Brand') {
+      $redirect('/brand?v=' + encodeURIComponent(filter.v));
+    } else if (filter.type == 'Keyword') {
+      $redirect('/keywords?v=' + encodeURIComponent(filter.v));
+    } else {
+      alert('Invalid filter');
     }
   };
 }
