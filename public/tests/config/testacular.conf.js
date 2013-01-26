d491553fd2fefd456e5b7ccde28d472a4b93d0ea
Valeri Karpov <valkar207@gmail.com>
2013-01-26 12:40:19 -0500

    Move angular testing libs into their own folder

--- public/tests/config/testacular.conf.js
+++ public/tests/config/testacular.conf.js
@@ -7,7 +7,7 @@ files = [
   'https://ajax.googleapis.com/ajax/libs/jqueryui/1.9.2/jquery-ui.min.js',
   'http://ajax.googleapis.com/ajax/libs/angularjs/1.0.3/angular.js',
   'vendor/angular/angular-*.js',
-  'tests/angular-mocks.js',
+  'tests/lib/angular-mocks.js',
   'javascripts/basic_tools.js',
   'javascripts/TaggerViewController.js',
   'javascripts/MainSearchBarController.js',
