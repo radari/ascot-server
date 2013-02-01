850fb0a923c19effba17772392dd343b21edf0be
Valeri Karpov <valkar207@gmail.com>
2013-02-01 00:29:13 -0500

    Add average method to Stopwatch class

--- public/common/Stopwatch.js
+++ public/common/Stopwatch.js
@@ -27,4 +27,15 @@ exports.Stopwatch = function() {
     this.tagToTimes[tag].push(t);
     return t;
   };
+
+  this.average = function(tag) {
+    if (!tag in this.tagToTimes) {
+      return -1;
+    }
+    var sum = 0;
+    for (var i = 0; i < this.tagToTimes[tag].length; ++i) {
+      sum += this.tagToTimes[tag][i];
+    }
+    return sum / this.tagToTimes[tag].length;
+  }
 };
