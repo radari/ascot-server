affb6aeac5de908060206bc544c08c3e2862e1b6
Matt Green <mgreen9@gmail.com>
2013-02-20 13:34:52 -0500

    added ng controller for admin

---
+++ public/javascripts/controllers/AdminController.js
@@ -0,0 +1,13 @@
+/*
+ *  MainSearchBarController.js
+ *
+ *  Created on: February 20, 2013
+ *      Author: Matt Green
+ *
+ *  Controller for admin panel
+ *
+ */
+
+function MainSearchBarController($scope){
+
+}
