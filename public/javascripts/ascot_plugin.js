30612509fbea9192501afe0238e7fcce82cc0684
Valeri Karpov <valkar207@gmail.com>
2013-03-05 11:38:14 -0500

    Clean up c8437cf. Consider this fix #140

--- public/javascripts/ascot_plugin.js
+++ public/javascripts/ascot_plugin.js
@@ -12,14 +12,13 @@
 
 
 function initAscotPlugin($, tagSourceUrl) {
-  if (typeof '_gaq' == 'undefined') {
+  if (!window._gaq) {
     // Insert Google Analytics if it doesn't already exist
     window._gaq = [];
     
     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-    alert('Inserted GA!');
   }
   
   _gaq.push(function() {
