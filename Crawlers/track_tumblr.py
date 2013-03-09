c895c2c41b5faffd75e583e4ae7e2b4ba871480d
Matt Green <mgreen9@gmail.com>
2013-03-09 17:51:37 -0500

    finished details tumblr crawler

--- Crawlers/track_tumblr.py
+++ Crawlers/track_tumblr.py
@@ -19,12 +19,13 @@ tumblr_base = "http://api.tumblr.com/v2/blog/"
 
 tumblr = httplib2.Http(".cache")
 
-report_file = open("report.tsv", "w")
+current_date = datetime.now()
+
+report_file = open(current_date.strftime('%Y-%m-%d') + "_report.tsv", "w")
+log_file = open('tumblr_crawler.log', 'a')
 config_file = open("tumblr_posts.txt")
 
-
 sep = "\t"
-current_date = datetime.now()
 
 # array of dictionaries containing the info about each post retrieved
 # will allow entries to be sorted in the future
@@ -36,20 +37,17 @@ report_file.writelines("Poster"+sep+
             "Tags"+sep+
             "Likes"+sep+
             "Reblogs"+sep+
-            "Followers at Time"+sep+
             "Ascot Link" +sep+
             "Tumblr Link\n")
 
 
+log_file.writelines(current_date.strftime('%Y-%m-%d %H:%M:%S') + ": Tracker started\n")
 
 tumblr_blogs = config_file.read().strip().split('#')
 tumblr_blogs.remove('')
 for blog in tumblr_blogs:
   post_list = blog.strip().split()
   blog_base = post_list.pop(0).strip()
-
-  print blog_base
-  print post_list
 
   for post_id in post_list:
     request_url = (tumblr_base + blog_base + "/posts?" + "api_key=" + client_key +
@@ -62,10 +60,9 @@ for blog in tumblr_blogs:
 
       # Short cut
       post = info['response']['posts'][0]
-      print(info['response']['blog']['name'] + " " + str(info['response']['blog']['likes']))
+
       likes = 0
       reblogs = 0
-      blog_followers = "NILL"
 
 
       if('notes' in post):
@@ -87,26 +84,26 @@ for blog in tumblr_blogs:
                         'tags': ','.join(post['tags']),
                         'likes': likes,
                         'reblogs': reblogs,
-                        'followers': blog_followers,
                         'ascot_url': ascot_url,
                         'tumblr_url': post["short_url"]})
-
-
+      post_info_string = u''
       post_info_string = (info['response']['blog']['name'] +sep+
                           post['date'] +sep+
                           ','.join(post['tags']) +sep+
                           str(likes) +sep+
                           str(reblogs) +sep+
-                          blog_followers +sep+
                           ascot_url +sep+
                           post["short_url"] + "\n")
 
-      report_file.writelines(post_info_string)
+      # Conver to ASCII and write out 
+      # Note: this may result in some character loss in the tags but hopefully it won't matter
+      report_file.writelines(post_info_string.encode('ascii', 'ignore'))
 
     else:
-      error_str = current_date + " Errror with " + request_url
+      error_str = "Errror with " + request_url
       print(error_str)
-      report_file.writelines(error_str + "\n")
+      log_file.writelines(error_str + "\n")
 
 
 
+log_file.writelines(current_date.strftime('%Y-%m-%d %H:%M:%S') + ": Tracker finished\n\n")
