31c2eee6d822c1bcfb8c1ac28072a643a413e644
Cesar Devers <cesar.devers@gmail.com>
2013-03-19 14:50:32 -0400

    finished signup/registration page

--- public/javascripts/navMagic.js
+++ public/javascripts/navMagic.js
@@ -9,3 +9,15 @@ $(document).ready(function () {
 		$(this).find('h3').fadeIn(200);
 	});
 });
+
+$(document).ready(function() {
+	$("#signUp").click(
+		function() {
+			$('#pickEmail').fadeIn(500);
+			$('.loginHead').fadeOut(10);
+			$('.signupHead').fadeIn(500);
+			$('.hideMe').fadeOut(10);
+			$('#createAccount').fadeIn(500);
+			$('#loginForm').find('#username').focus();
+		});
+});
