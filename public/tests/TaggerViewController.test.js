35de58ae793143e3be129a3cf9ec72be27793a50
Valeri Karpov <valkar207@gmail.com>
2013-03-26 16:37:05 -0400

    Quick fix for testacular tests

--- public/tests/TaggerViewController.test.js
+++ public/tests/TaggerViewController.test.js
@@ -158,8 +158,8 @@ describe('TaggerViewController', function() {
     $httpBackend.flush();
     var tag1 = scope.addTag('1234', 25, 15);
 
-    $httpBackend.expectPUT('/tagger/5678/1234').respond({});
-    scope.finalize('1234', '5678');
+    $httpBackend.expectPUT('/tagger/1234').respond({});
+    scope.finalize('1234');
     $httpBackend.flush();
     expect(url).toBe('/look/1234?showProgress=1');
   });
