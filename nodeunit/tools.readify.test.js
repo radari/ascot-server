be0247eec70c11a38cc5e58492b3af1907a5674c
Valeri Karpov <valkar207@gmail.com>
2013-04-24 18:05:36 -0400

    Readify now generally works

--- nodeunit/tools.readify.test.js
+++ nodeunit/tools.readify.test.js
@@ -8,10 +8,41 @@
  *
  */
 
-var readify = require('../routes/tools/readify.js').readify();
+var Readify = require('../routes/tools/readify.js').readify;
 
 exports.testOutput = function(test) {
-  var z = readify({ brand : 'Nike', name : 'AIR ZOOm 3'});
-  test.equal(z, 'Nike-Air-Zoom-3');
-  test.done();
+  var mockProduct = {
+    brand : 'Nike',
+    name : 'AIR ZOOM 3'
+  };
+
+  var mockResult = 'Nike-Air-Zoom-3';
+  var mockUrl = 'http://nike.tk/longUrlHere';
+
+  var Readable = function(object) {
+    test.equal(object.readable, mockResult);
+    test.equal(object.number, 3);
+    test.equal(object.url, mockUrl);
+
+    this.readable = object.readable;
+    this.number = object.number;
+    this.url = object.url;
+
+    this.save = function(callback) {
+      callback(null, this);
+    };
+  };
+
+  Readable.find = function(query, callback) {
+    test.equal(query.readable, mockResult);
+    callback(null, [1, 2]);
+  };
+
+  var readify = Readify(Readable, 'http://test');
+
+  readify(mockProduct, mockUrl, function(error, result) {
+    test.equal('http://test/p/Nike-Air-Zoom-3/3', result);
+    test.expect(5);
+    test.done();
+  });
 };
--- routes/tools/readify.js
+++ routes/tools/readify.js
@@ -5,12 +5,13 @@
  *      Author: Valeri Karpov
  *
  *  Create readable links for our product links because Apple is a steaming
- *  pile of buffalo feces.
+ *  pile of buffalo feces. Basically creates a product link like
+ *  http://www.ascotproject.com/p/Nike-Air-Zoom-3/1
  *
  */
 
-exports.readify = function() {
-  return function(product, callback) {
+exports.readify = function(Readable, myUrl) {
+  return function(product, url, callback) {
     var s = product.brand + ' ' + product.name;
     var sp = s.split(/\s+/);
     var ret = "";
@@ -19,6 +20,15 @@ exports.readify = function() {
           sp[i].charAt(0).toUpperCase() + sp[i].substr(1).toLowerCase();
     }
 
-    return ret;
+    Readable.find({ readable : ret }, function(error, readables) {
+      var r = new Readable({ readable : ret, number : readables.length + 1, url : url });
+      r.save(function(error, r) {
+        if (error || !r) {
+          callback("error - " + error, null);
+        } else {
+          callback(null, myUrl + '/p/' + r.readable + '/' + r.number);
+        }
+      });
+    });
   };
 };
