3ca21726615743e8c6ad8e79f4419ec1fc03bbf4
Cesar Devers <cesar.devers@gmail.com>
2013-05-07 12:42:21 -0400

    colorpicker minicolors added to customize view

--- public/javascripts/controllers/customOverlayController.js
+++ public/javascripts/controllers/customOverlayController.js
@@ -1,28 +1,38 @@
 function customOverlayController($scope) {
+		$scope.tagStyleOptions=[{
+				name:'Bordered',
+				isChecked:'true'
+			},
+			{
+				name:'Numbered',
+				isChecked:'false'
+			}]
+
+		$scope.checkIfCheckedBordered= function () {
+			alert('this is being called');
+		}
+		$('INPUT.minicolors').minicolors({opacity:true});
 		
-	
-$scope.options=[{
-		name:'Bordered',
-		isChecked:'true'
-	},
-	{
-		name:'Numbered',
-		isChecked:'false'
-	}]
-	$scope.checkIfChecked= function() {
-		if ($scope.options[0].isChecked==true) {
-			$('.exampleTag').addClass("bordered");
+
+		$scope.changeColor= function() {
+			var colorChosen= $('INPUT.minicolors').minicolors('rgbaString')
+			$('.exampleTag').css('background-color', colorChosen);
 		}
-		else if ($scope.options[0].isChecked==false) {
-			$('.exampleTag').removeClass("bordered");
-		}
-		else if ($scope.options[1].isChecked==true) {
-			$('.exampleTag').css( "color", "white" );
-		}
-		else if ($scope.options[1].isChecked==false) { 
-			$('.exampleTag').css( "color", "#171717" );
-		}
-		else {
+
+		$scope.checkIfChecked= function() {
+			if ($scope.tagStyleOptions[0].isChecked==true) {
+				$('.exampleTag').addClass("bordered");
 			}
-	}
+			else if ($scope.tagStyleOptions[0].isChecked==false) {
+				$('.exampleTag').removeClass("bordered");
+			}
+			else if ($scope.tagStyleOptions[1].isChecked==true) {
+				$('.exampleTag').addClass( "numbered");
+			}
+			else if ($scope.tagStyleOptions[1].isChecked==false) { 
+				$('.exampleTag').removeClass("numbered");
+			}
+			else {
+				}
+		}
 }
