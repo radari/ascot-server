dadb33cb5c8123a548be615f19a6b1edee6a83ce
Cesar Devers <cesar.devers@gmail.com>
2013-05-08 13:42:31 -0400

    additonal logic for bordeder toggle

--- public/javascripts/controllers/customOverlayController.js
+++ public/javascripts/controllers/customOverlayController.js
@@ -11,11 +11,17 @@ function customOverlayController($scope) {
 		$(".borderToggle").toggle( function() {
 			$(this).css("text-decoration", "line-through");
 			$(this).css("opacity","0.5")
-			$('.exampleTag').addClass("bordered");
+			$( ".slider" ).slider( "disable" );
+			$('.sliderValue').css("opacity", "0.5");
+			$('.exampleTag').css("border", "none");
+
 		}, function() {
 			$(this).css("text-decoration", "none");
 			$(this).css("opacity","1.0")
-			$('.exampleTag').removeClass("bordered");
+			$( ".slider" ).slider( "enable" );
+			$('.sliderValue').css("opacity", "1");
+			$('.exampleTag').addClass("bordered");
+			checkValue();
 		});
 		//show sub menu
 		$(".customizeToolbar li:nth-child(1)").hover(function() {
@@ -42,32 +48,32 @@ function customOverlayController($scope) {
 		$('.animationOptions').mouseleave(function() {
 			$(this).fadeOut(100);
 		})
+		//prevent more than one submenu from being open 
+		
 
-		$scope.checkIfCheckedBordered= function () {
-			alert('this is being called');
-		}
+		function checkValue() {
+				var sliderVal= $(".slider").slider("value");
+				$('.sliderValue').html(sliderVal+"px");
+				$('.exampleTag').css("border", sliderVal+'px white solid')
+			}
+		
 		$('INPUT.minicolors').minicolors({opacity:true});
-		$('.slider').slider();
+		$('INPUT.minicolors2').minicolors({opacity:true});
+		$('.slider').slider({
+			min:1, 
+			max:5,
+			value:2,
+			slide: function(event, ui) {
+				var sliderVal= $(".slider").slider("value");
+				$('.sliderValue').html(sliderVal+"px");
+				$('.exampleTag').css("border", sliderVal+'px white solid')
+			}
+		});
 
 		$scope.changeColor= function() {
-			var colorChosen= $('INPUT.minicolors').minicolors('rgbaString')
+			var colorChosen= $('INPUT.minicolors').minicolors('rgbaString');
+			var colorChosen2= $('INPUT.minicolors2').minicolors('rgbaString');
 			$('.exampleTag').css('background-color', colorChosen);
-		}
-
-		$scope.checkIfChecked= function() {
-			if ($scope.tagStyleOptions[0].isChecked==true) {
-				
-			}
-			else if ($scope.tagStyleOptions[0].isChecked==false) {
-				$('.exampleTag').removeClass("bordered");
-			}
-			else if ($scope.tagStyleOptions[1].isChecked==true) {
-				$('.exampleTag').addClass( "numbered");
-			}
-			else if ($scope.tagStyleOptions[1].isChecked==false) { 
-				$('.exampleTag').removeClass("numbered");
-			}
-			else {
-				}
+			$('.exampleTag').css('color', colorChosen2);
 		}
 }
