d7a9f370144ac9e2ea2019fb534cdd6527a8ebfd
Valeri Karpov <valkar207@gmail.com>
2013-06-06 11:55:12 -0400

    Tests for CollectionController done for now

--- public/javascripts/controllers/CollectionController.js
+++ public/javascripts/controllers/CollectionController.js
@@ -28,8 +28,8 @@ function CollectionController($scope, $http, $window, $timeout, $windowSize) {
   var oldHeight = 0;
   var oldWidth = 0;
   $scope.getImageStyle = function(look) {
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
+    var windowHeight = $windowSize.getWindowSize().height;
+    var windowWidth = $windowSize.getWindowSize().width;
     var fullscreenWidth = (windowHeight / look.size.height) * look.size.width;
     
     // Make sure that we notify of image resize
@@ -46,18 +46,6 @@ function CollectionController($scope, $http, $window, $timeout, $windowSize) {
     } else {
       var h = Math.floor((windowWidth / look.size.width) * look.size.height);
       return { height : h + 'px', 'margin-top' : '0px', width : windowWidth + 'px' };
-    }
-  };
-
-  $scope.getImageMarginTop = function(look) {
-    var windowHeight = $(window).height();
-    var windowWidth = $(window).width();
-    var fullscreenWidth = (windowHeight / look.size.height) * look.size.width;
-    if (fullscreenWidth < windowWidth) {
-      return 0;
-    } else {
-      var h = Math.floor((windowWidth / look.size.width) * look.size.height);
-      return Math.floor((windowHeight - h) / 2);
     }
   };
 }
--- public/tests/CollectionController.test.js
+++ public/tests/CollectionController.test.js
@@ -9,11 +9,13 @@
  */
 
 describe('CollectionController', function() {
-  var scope, v, $httpBackend, resizeCount;
+  var scope, v, $httpBackend, resizeCount, $timeout, $windowSize;
+  var h = 200, w = 200;
 
   beforeEach(inject(function($injector, $rootScope, $controller) {
     $httpBackend = $injector.get('$httpBackend');
     $window = $injector.get('$window');
+    $timeout = $injector.get('$timeout');
     scope = $rootScope.$new();
     resizeCount = 0;
     
@@ -22,12 +24,49 @@ describe('CollectionController', function() {
         ++resizeCount;
       }
     };
+    
+    $windowSize = {
+      getWindowSize : function() {
+        return { height : h, width : w };
+      },
+      resize : function(callback) {
+        // Do nothing
+      }
+    };
 
     v = $controller(CollectionController,
-        { $scope : scope, $window : $window });
+        { $scope : scope, $window : $window, $windowSize : $windowSize });
   }));
   
-  it('should get style properly', function() {
+  it('should be able to get style', function() {
+    var ret = scope.getImageStyle({ size : { height : 100, width : 100 } });
+    expect(ret.height).toBe('200px');
+    expect(ret['margin-top']).toBe('0px');
+    expect(ret.width).toBe('200px');
+  });
+  
+  it('should tell plugin to resize all on new height and width', function() {
+    var ret = scope.getImageStyle({ size : { height : 100, width : 100 } });
+    $timeout.flush();
+    expect(resizeCount).toBe(1);
     
+    h = 300;
+    w = 400;
+    var ret = scope.getImageStyle({ size : { height : 100, width : 100 } });
+    expect(ret.height).toBe('300px');
+    expect(ret['margin-top']).toBe('0px');
+    expect(ret.width).toBe('300px');
+    $timeout.flush();
+    expect(resizeCount).toBe(2);
+  });
+  
+  it('should not be entire screen height when screen is too narrow', function() {
+    h = 300;
+    w = 200;
+    
+    var ret = scope.getImageStyle({ size : { height : 100, width : 100 } });
+    expect(ret.height).toBe('200px');
+    expect(ret['margin-top']).toBe('0px');
+    expect(ret.width).toBe('200px');
   });
 });
