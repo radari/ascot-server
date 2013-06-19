2839bed82503cdb90cfd8802dbcbc51d83e96b1a
Valeri Karpov <valkar207@gmail.com>
2013-06-18 22:41:33 -0400

    Tweak adjust_wrappers

--- public/javascripts/adjust_wrappers.js
+++ public/javascripts/adjust_wrappers.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function() {
   $("img[adjust_ascot_wrapper]").each(function(i, el) {
-    $(el).load(function() {
+    $(el).imagesLoaded(function() {
       $(el).parent().css('width', $(el).width() + 'px');
       $(el).parent().css('height', $(el).height() + 'px');
     });
